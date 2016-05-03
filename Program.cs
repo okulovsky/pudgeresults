a148971be9e522db2eeb5b579473520f945b4e59
okulovsky <yuri.okulovsky@gmail.com>
2016-05-03 15:40:00 +0600

    Final results

--- Program.cs
+++ Program.cs
@@ -9,20 +9,20 @@ namespace ConsoleApplication3
 {
 	class Program
 	{
-		static void Add(Dictionary<string,int> names, string name, int toAdd)
+		static void Add(Dictionary<string,int[]> names, string name, int index, int toAdd)
 		{
 			name = name.Replace("ё", "е").Replace(",", "");
 			if (string.IsNullOrWhiteSpace(name)) return;
 			if (names.ContainsKey(name))
 			{
-				names[name] += toAdd;
+				names[name][index] += toAdd;
 				return;
 			}
 			var found = names.Keys.Where(z => z.StartsWith(name) || name.StartsWith(z)).FirstOrDefault();
 
 			if (found!=null)
 			{
-				names[found] += toAdd;
+                names[found][index] += toAdd;
 				return;
 			}
 
@@ -32,7 +32,7 @@ namespace ConsoleApplication3
 				var rename = fio[1] + " " + fio[0];
 				if (names.ContainsKey(rename))
 				{
-					names[rename] += toAdd;
+                    names[rename][index] += toAdd;
 					return;
 				}
 			}
@@ -43,7 +43,7 @@ namespace ConsoleApplication3
 		[STAThread]
 		static void Main(string[] args)
 		{
-			var names = File.ReadLines("allnames.txt").OrderBy(z => z).ToDictionary(z => z, z => 0);
+			var names = File.ReadLines("allnames.txt").OrderBy(z => z).Select(z=>z.Replace('ё','е')).ToDictionary(z => z, z => new int[3]);
 			var registrations = File
 				.ReadLines("registrations.tsv")
 				.Skip(1)
@@ -59,6 +59,10 @@ namespace ConsoleApplication3
 				.Skip(1)
 				.Select(z => z.Split('\t'))
 				.ToDictionary(z => z[0].ToLower(), z => int.Parse(z[5]));
+            var games3 = File.ReadAllLines("games3.tsv")
+                .Skip(1)
+                .Select(z => z.Split('\t'))
+                .ToDictionary(z => z[0].ToLower(), z => int.Parse(z[3]));
 
 			foreach (var e in games1)
 				registrations[e.Key].Scores[0] = e.Value;
@@ -66,16 +70,19 @@ namespace ConsoleApplication3
 			foreach (var e in games2)
 				registrations[e.Key].Scores[1] = e.Value;
 
-			foreach (var e in registrations)
-			{
-				Add(names,e.Value.First, e.Value.Scores.Sum());
-				Add(names, e.Value.Second, e.Value.Scores.Sum());
-				
-			}
+            foreach (var e in games3)
+                registrations[e.Key].Scores[2] = e.Value;
 
-			var result = names.OrderBy(z => z.Key).Select(z => z.Value.ToString()).Aggregate((a, b) => a + "\n" + b);
+			foreach (var e in registrations)
+                for (int i = 0; i < e.Value.Scores.Length; i++)
+                {
+                    Add(names, e.Value.First, i, e.Value.Scores[i]);
+                    Add(names, e.Value.Second,i, e.Value.Scores[i]);
+                }
+			var result = names.OrderBy(z => z.Key).Select(z => z.Key+"\t"+z.Value[0]+"\t"+z.Value[1]+"\t"+z.Value[2]).Aggregate((a, b) => a + "\n" + b);
 
 			System.Windows.Forms.Clipboard.SetText(result);
+            Console.WriteLine(result);
 		}
 
 	}
